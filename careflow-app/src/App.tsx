67dc48ffb0d35d76805deee8c2fab56ecda915df
christoffer <chriistoffer.osterberg@gmail.com>
2023-09-28 22:31:05 +0200

    added firebase config

--- careflow-app/src/App.tsx
+++ careflow-app/src/App.tsx
@@ -6,11 +6,25 @@ import NavigationBar from './components/Navbar';
 import Start from './components/Start';
 import Login from './components/Login'
 import Projects from './components/Projects';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 function App() {
+  // firebase stuff...
+  const firebaseConfig = {
+    apiKey: "AIzaSyD_VmL7OhFcnqzzsmyowA4v0aMHf2MVXDs",
+    authDomain: "careflow-60a15.firebaseapp.com",
+    projectId: "careflow-60a15",
+    storageBucket: "careflow-60a15.appspot.com",
+    messagingSenderId: "100218090999",
+    appId: "1:100218090999:web:fef937f15602aa48a0aaf2"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  // the app...
   return (
     <>
-      <NavigationBar/>
+      <NavigationBar />
       <div>
         <Routes>
           <Route path="/" element={<Login />} />
