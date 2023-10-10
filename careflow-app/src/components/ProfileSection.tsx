83bca313fcd4db75d30032ec2218c5a8932239cc
Filip Nyström <filny739@student.liu.se>
2023-10-10 13:53:43 +0200

    ändrade profile section

--- careflow-app/src/components/ProfileSection.tsx
+++ careflow-app/src/components/ProfileSection.tsx
@@ -2,11 +2,20 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function ProfileSection() {
+    const profileSectionStyle = {
+        backgroundColor: "lightblue",
+        width: "500px",
+        height: "250px",
+        borderRadius: "10px",
+    };
+
     return (
-        <div>
-          <h1>Profile Section</h1>
+        <div style={profileSectionStyle}>
+            <h1>Profile Section</h1>
+            <h3>Namn</h3>
+            <h3>Avdelning</h3>
         </div>
-      );
+    );
 }
 
 export default ProfileSection;
