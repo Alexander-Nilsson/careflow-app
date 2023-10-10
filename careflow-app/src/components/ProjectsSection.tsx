46473c9dfc80fa6e5cd45be08ecfe244f303dac1
Filip Nyström <filny739@student.liu.se>
2023-10-10 14:20:46 +0200

    fil kom inte med innan

---
+++ careflow-app/src/components/ProjectsSection.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function ProjectsSection() {
+    // You can define your project data here
+    const projectsSectionStyle = {
+        backgroundColor: "lightblue",
+        width: "1000px",
+        height: "250px",
+        borderRadius: "10px",
+        margin: "20px",
+    };
+
+    return (
+        <div style={projectsSectionStyle}>
+            <h1>Project Section</h1>
+            
+        </div>
+    );
+}
+
+export default ProjectsSection;
