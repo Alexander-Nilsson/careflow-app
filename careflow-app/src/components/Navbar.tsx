ff646b528e9d0c0bf3360226efeed81e0152a57a
Julia Jaksic <julja673@student.liu.se>
2023-10-12 14:14:09 +0200

    tried to fix problem with log out

--- careflow-app/src/components/Navbar.tsx
+++ careflow-app/src/components/Navbar.tsx
@@ -1,40 +1,28 @@
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
-import { signedIn, signOutUser } from '../firebase';
 import { useAuth0 } from '@auth0/auth0-react';
-// import clientID from '../auth0';
-
 
 function NavigationBar() {
-
-  const navigate = useNavigate();
+  const { isAuthenticated, logout } = useAuth0();
 
   const linkStyle = {
     color: 'white',
-    fontSize: '24px', // Change to your desired font size
+    fontSize: '24px',
     padding: '1em',
     fontWeight: 'bold',
     textDecoration: 'none',
   };
 
-  const { logout, isAuthenticated} = useAuth0();
-
-  // logout({
-  //   "federated": true,
-  // });
-
-  async function handleLogout(event: React.MouseEvent<HTMLAnchorElement>) {
-    event.preventDefault()
-    // window.location.href = `https://dev-ni7jkmfx0oybqzdf.us.auth0.com/v2/logout?client_id=mkmVvsZQwku14qYIH34tA4kPKdTzejse&returnTo=${encodeURIComponent(window.location.origin)}`
-    // await signOutUser()
-    // navigate("/login")
-  }
+  const handleLogout = () => {
+    if (isAuthenticated) {
+      logout(); // Just initiate the logout process, no need for 'returnTo'
+    }
+  };
 
   return (
-    <Navbar expand="lg" variant="dark" className="d-flex" style={{ backgroundColor: '#0a206a' }} >
-      <Link to="/start">
+    <Navbar expand="lg" variant="dark" className="d-flex" style={{ backgroundColor: '#0a206a' }}>
+      <Link to="/">
         <img
           alt=""
           src="CareFlow_Vit.png"
@@ -46,14 +34,15 @@ function NavigationBar() {
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav" className='d-flex'>
         <Nav className="d-flex flex-grow-1">
-          <Link to="/start" style={linkStyle} >Start</Link>
-          <Link to="/forandringsarbeten" style={linkStyle} >Förändringar</Link>
-          <Link to="/arkiv" style={linkStyle} >Arkiv</Link>
-          <Link to="/guide" className="flex-grow-1" style={linkStyle} >Guide</Link>
-          <a href="/" style={linkStyle} onClick={() => logout()}>Logga ut</a>
+          <Link to="/" style={linkStyle} >Home</Link>
+          <Link to="/profile" style={linkStyle} >Profile</Link>
+          {isAuthenticated && (
+            <button onClick={handleLogout} style={linkStyle}>Log Out</button>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 }
+
 export default NavigationBar;
