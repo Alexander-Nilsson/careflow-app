2cf67803b3aa2a2d4d082bd8e4315febc1bc54c8
Filip Nyström <filny739@student.liu.se>
2023-10-30 12:23:03 +0100

    added circle for profile pic

--- careflow-app/src/components/ProfileSection.tsx
+++ careflow-app/src/components/ProfileSection.tsx
@@ -3,29 +3,50 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CreateNewProject from "./CreateNewProject";
 
 function ProfileSection() {
+  const profileSectionStyle = {
+    backgroundColor: "lightblue",
+    width: "500px",
+    height: "250px",
+    borderRadius: "10px",
+    margin: "20px",
+    display: "flex", 
+  };
 
-  
+  const leftDivStyle = {
+    flex: "40%", 
+    padding: "10px",
+  };
 
+  const rightDivStyle = {
+    flex: "60%", 
+    padding: "10px",
+  };
 
+  const circleStyle = {
+    width: "150px",
+    height: "150px",
+    backgroundColor: "white",
+    borderRadius: "50%", // Create a circular shape
+    display: "flex",
+    alignItems: "center", // Center vertically
+    justifyContent: "center", // Center horizontally
+    marginLeft: "15px",
+    marginTop: "10px",
+  };
 
-    const profileSectionStyle = {
-        backgroundColor: "lightblue",
-        width: "500px",
-        height: "250px",
-        borderRadius: "10px",
-        margin: "20px",
-    };
-
-
-    return (
-        <div style={profileSectionStyle}>
-            <h1>Min Profil</h1>
-            <h3>Namn</h3>
-            <h3>Avdelning</h3>
-            <CreateNewProject />
-
-        </div>
-    );
+  return (
+    <div style={profileSectionStyle}>
+      <div style={leftDivStyle}>
+        <div style={circleStyle}></div>
+      </div>
+      <div style={rightDivStyle}>
+        <h1>Min Profil</h1>
+        <h3>Namn</h3>
+        <h3>Avdelning</h3>
+        <CreateNewProject />
+      </div>
+    </div>
+  );
 }
 
 export default ProfileSection;
