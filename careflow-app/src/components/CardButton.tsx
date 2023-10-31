25bb1e9812b9e7f7e5edbf6b944d742de74dc5e4
Niklas <nikjo597@student.liu.se>
2023-10-31 14:54:07 +0100

    fixing dynamic containers

--- careflow-app/src/components/CardButton.tsx
+++ careflow-app/src/components/CardButton.tsx
@@ -1,5 +1,6 @@
 import Card from "react-bootstrap/Card";
 import { PersonFill } from "react-bootstrap-icons";
+import "./CardButton.css";
 
 interface CardButtonProps {
   title: string;
@@ -14,21 +15,31 @@ function CardButton({ title, tags, onClick }: CardButtonProps) {
       onClick={onClick}
       style={{ cursor: "pointer", textDecoration: "none" }}
     >
+      {/* Kom ihåh att ändra CSS storleken om ni ändrar style size */}
       <Card
         style={{
           width: "18vw",
           maxWidth: "300px",
           minWidth: "150px",
+
           margin: "1vw",
         }}
       >
         <Card.Body>
           <Card.Title>
-            <div> tags</div>
-            <div> {"   " + title}</div>
-            <div> Datum</div>
-            <div> ikoner</div>
-            <PersonFill />
+            <div className="outerContainer">
+              <div className="tags"> tags</div>
+              <div className="title"> {"   " + title}</div>
+              <div className="bottomContainer">
+                <div className="dateAndIcons">
+                  <div className="date">Date</div>
+                  <div className="icons">Icon</div>
+                </div>
+                <div className="profilCard">
+                  <PersonFill />
+                </div>
+              </div>
+            </div>
           </Card.Title>
           <Card.Text>
             {
