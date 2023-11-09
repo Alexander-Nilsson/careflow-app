720694a0ca43c09e274906cbc3172a5adebee45a
Emil Imhagen <emiim362@student.liu.se>
2023-11-09 08:48:44 +0100

    Verify that auth0 is used.

--- careflow-app/src/components/__tests__/login.test.tsx
+++ careflow-app/src/components/__tests__/login.test.tsx
@@ -14,7 +14,7 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('TC-login-8 (FR-010301)', () => {
-  test('redirects to /start after successful login', () => {
+  it('should redirect to /start after successful login', () => {
     const navigate = jest.fn(); // Mock the navigate function
 
     // Replace the mocked useNavigate with the new implementation in the test
@@ -28,5 +28,14 @@ describe('TC-login-8 (FR-010301)', () => {
 
     // If the user is authenticated, it should navigate to '/start'
     expect(navigate).toHaveBeenCalledWith('/start');
+  });
+});
+
+import fs from 'fs';
+
+describe('TC-login-10 (FR-010103)', () => {
+  it('should use useAuth0 hook', () => {
+    const loginComponent = fs.readFileSync('src/components/Login.tsx', 'utf8');
+    expect(loginComponent).toContain('useAuth0(');
   });
 });
